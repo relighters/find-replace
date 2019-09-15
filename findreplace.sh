248d539f53085c4c784a23253a9e8a533dc7fcf2
Relighters <git@rayoflight.cc>
2019-09-14 20:40:25 -0700

    Added basic script functionality with option flags

--- findreplace.sh
+++ findreplace.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Option flag parsing using bash's getopts function
+while getopts ":f:r:" option; do
+    case "${option}" in
+        f) f=${OPTARG} ;;       # enables "-f" flag for specifying search string
+        r) r=${OPTARG} ;;       # enables "-r" flag for specifying replace string
+    esac
+done
+
+# Filename parsing and move [rename] operations
+for file in * ; do
+    mv $file ${file/$f/$r} ;
+done

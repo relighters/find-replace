2e5fb6f87a37e26c82dec6ba38c76c507ffcf180
Relighters <git@rayoflight.cc>
2019-09-16 22:41:19 -0700

    Added search preview and Y/n confirmation dialogue

--- findreplace.sh
+++ findreplace.sh
@@ -10,10 +10,28 @@ while getopts ":f:r:" option; do
     esac
 done
 
+# Filename search preview (will add error msg if no result found in future)
+ls | grep -i --color ${f}
+
+# Echos used to skip one line for confirmation legibility
+echo
+
+# Filename modification confirmation (Y to continue, n to exit)
+read -p "Modify by replacing [${f}] with [${r}]? (Y/n):" -n 1 -r
+
+# Echos used to skip two lines for confirmation legibility
+echo
+echo
+
+# Confirmation, with continue on Y(es) response
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
 # Filename parsing and move [i.e. rename] operations
 for file in *.!(sh) ; do
-    mv $file ${file/$f/$r}
+    mv -v $file ${file/$f/$r}
 done
+fi
 
 # Disables shopt to return it back to it's default state (off)
 shopt -u extglob
